6e439b3760402803483514618da19ffc9c91eb3d
Tanay <tanay.patel@student.csulb.edu>
2020-10-30 13:11:42 +0530

    commit3

--- task1.py
+++ task1.py
@@ -1,33 +1,33 @@
-import sys
-records=0
-sum_score=0
-avg=0
-score_list=[]
-READ_FILE='scores.txt'
+import sys  #importing sys module
+records=0   #variable to store student records
+sum_score=0 #variable to store sum of scores of all students
+avg=0       #variable to store average score
+score_list=[] # list to store score values
+READ_FILE='scores.txt' 
 WRITE_FILE='log.txt'
 read_mode='r'
 write_mode='w'
 
 try:
-        score=open(READ_FILE,read_mode)
-        log=open(WRITE_FILE,write_mode)
+        score=open(READ_FILE,read_mode)# opening scores.txt
+        log=open(WRITE_FILE,write_mode)# opening log.txt
         with score,log:
-                for line in score:
-                        name,score=line.split()
+                for line in score:      # loop for each line in read file
+                        name,score=line.split() #splits the string at whitespace
                         try:          
-                                score_list.append(int(score))
-                        except ValueError as error:
+                                score_list.append(int(score)) #score from string is appended into list
+                        except ValueError as error: #passing value error exception
                                 log.write(f'Bad score value for {name}, ignored.\n')
                                 print(f'Bad score value for {name}, ignored.')
                         else:
-                                records+=1
-                avg=int(sum(score_list)/len(score_list))
+                                records+=1 #incrementing record by 1
+                avg=int(sum(score_list)/len(score_list))# calculating average score
                 
 
-                log.write(f'The class average is {avg} for {records} students.\n')
+                log.write(f'The class average is {avg} for {records} students.\n')# writing output to log file
                 print('The class average is',avg,'for',records,'students')
 
-except OSError as error:
+except OSError as error:# passing the OS error exception
         print('Unable to open',READ_FILE,'.Error Message:',error)
         sys.exit(0)
 
--- task2.py
+++ task2.py
@@ -1,5 +1,5 @@
-import sys
-from collections import Counter
+import sys #importing sys module
+from collections import Counter #importing unordered 
 
 READ_FILE='book.txt'
 WRITE_FILE='summary.txt'
@@ -8,27 +8,27 @@ write_mode='w'
 
 
 try:
-    book=open(READ_FILE,read_mode)
-    summary=open(WRITE_FILE,write_mode)
+    book=open(READ_FILE,read_mode)# opening book file
+    summary=open(WRITE_FILE,write_mode)# opening summary file
     
-    content=book.readline()
-    results=Counter(content.lower())
+    content=book.readline()# to read the content of file in one operation
+    result_value=Counter(content.upper())# storing the elements as dictionary keys and their counts as dictionary values
 
     with book,summary:
-        counter=0
-        for item in sorted(results.items()):
-            if item[0].isalpha():
-                counter+=1
-                summary.write(f'{str(item[0]).capitalize()} {str(item[1])}\n')
-                print(f'{str(item[0]).capitalize()} {str(item[1])}')
+        uniq_alpha_count=0 # variable to count unique alphabets
+        for i in sorted(result_value.items()):#perform sorting of the dict values
+            if i[0].isalpha(): # checks if the characters are alphabets and returns True
+                uniq_alpha_count+=1 #incrementing the variable
+                summary.write(f'{str(i[0])} {str(i[1])}\n') # writing the output to summary file
+                print(f'{str(i[0])} {str(i[1])}')
         
-        if counter==26:
-            summary.write(" It has all letters")
+        if uniq_alpha_count==26:
+            summary.write(" It has all letters")# writing the output to summary file
             print(" It has all letters")
         else:
-            summary.write("It doesnot have all letters")
+            summary.write("It doesnot have all letters")# writing the output to summary file
             print("It doesnot have all letters")
 
-except OSError as error:
+except OSError as error:#passing the OSError exception
     print('Unable to open',READ_FILE,'.Error Message:',error)
     sys.exit(0)           
